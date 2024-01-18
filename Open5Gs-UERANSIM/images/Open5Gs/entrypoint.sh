3dd9560558800e6d9ee8db2d781acee6a8e9b3c8
MuhLitfan <muhlitfan88@gmail.com>
2024-01-18 06:42:52 +0000

    update open5Gs Image Entrypoint

--- Open5Gs-UERANSIM/images/Open5Gs/entrypoint.sh
+++ Open5Gs-UERANSIM/images/Open5Gs/entrypoint.sh
@@ -8,20 +8,35 @@ function tun_create {
     if ! grep "ogstun" /proc/net/dev > /dev/null; then
         echo "Creating ogstun device"
         ip tuntap add name ogstun mode tun
+        ip tuntap add name ogstun2 mode tun
+        ip tuntap add name ogstun3 mode tun
     fi
 
-    ip addr del $IPV4_TUN_ADDR dev ogstun 2> /dev/null || true
-    ip addr add $IPV4_TUN_ADDR dev ogstun;
+    ip addr del 10.45.0.1/16 dev ogstun 2> /dev/null
+    ip addr add 10.45.0.1/16 dev ogstun
+    ip addr del 2001:db8:cafe::1/48 dev ogstun 2> /dev/null
+    ip addr add 2001:db8:cafe::1/48 dev ogstun
+    ip addr del 10.46.0.1/16 dev ogstun2 2> /dev/null
+    ip addr add 10.46.0.1/16 dev ogstun2
+    ip addr del 2001:db8:babe::1/48 dev ogstun2 2> /dev/null
+    ip addr add 2001:db8:babe::1/48 dev ogstun2
+    ip addr del 10.47.0.1/16 dev ogstun3 2> /dev/null
+    ip addr add 10.47.0.1/16 dev ogstun3
+    ip addr del 2001:db8:face::1/48 dev ogstun3 2> /dev/null
+    ip addr add 2001:db8:face::1/48 dev ogstun3
 
     sysctl -w net.ipv6.conf.all.disable_ipv6=0;         
-    ip addr del $IPV6_TUN_ADDR dev ogstun 2> /dev/null || true
-    ip addr add $IPV6_TUN_ADDR dev ogstun
     
     ip link set ogstun up
+    ip link set ogstun2 up
+    ip link set ogstun3 up
+
     sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward";
-    if [ "$ENABLE_NAT" = true ] ; then
-      iptables -t nat -A POSTROUTING -s $IPV4_TUN_SUBNET ! -o ogstun -j MASQUERADE;
-    fi
+
+
+    iptables -t nat -A POSTROUTING -s 10.45.0.0/16 ! -o ogstun -j MASQUERADE
+    iptables -t nat -A POSTROUTING -s 10.46.0.0/16 ! -o ogstun2 -j MASQUERADE
+    iptables -t nat -A POSTROUTING -s 10.47.0.0/16 ! -o ogstun3 -j MASQUERADE
 }
  
  COMMAND=$1
